18d6e963371eaee8631de363447264a50f751ab2
PaulDanPhillips <paul.dan.phillips@gmail.com>
2023-04-25 08:43:09 -0700

    Prelim Tuning NonResampled

---
+++ data/RAW/NonResampled/IgM/ModelTuning.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=BurkPx
+##SBATCH --output=
+#SBATCH --array=1-4
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=5000
+
+name1=$(sed -n "$SLURM_ARRAY_TASK_ID"p FileLST.txt)
+#name2=$(sed -n "$SLURM_ARRAY_TASK_ID"p Responses.txt)
+#redux=$(echo $name2 | cut -f 1 -d '.')
+
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+
+srun python ModelTuning.py -f $name1 -k 250 -a balanced_accuracy -o Results
